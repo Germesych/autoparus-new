b6f6431012da48c6405de6ee0aac36404c337e29
Aleksandr <kaiot1984@gmail.com>
2021-07-26 05:32:14 +0300

    add video modul

--- js/index.js
+++ js/index.js
@@ -1,90 +1,101 @@
+
+window.addEventListener('DOMContentLoaded', () => {
 // Video modal
-function wsVideoModalInit(opnions){
-	const id = options[0].src
-	let widthVideo = null
-	let heightVideo = null
+    const modalVideoConstructor = function(options) {
 
-	// Рендер модального окна
-	function _renderModal(idVideo, widthVideo, heightVideo){
-		const $body = document.querySelector('body')
-		$body.classList.add('hiden')
-		videoBlock = ` 
-			<div class="ws-modal__overlay">
-            <span class="ws-modal__close">&#10006;</span>
-            <iframe class="ws-modal__block" style="width: ${widthVideo || '870px'}; height: ${heightVideo || '500px'};" src="https://www.youtube.com/embed/${id || ''}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-		`
-		let div = document.createElement('div')
-  	div.className = "ws-modal__video"
-  	div.innerHTML = `${videoBlock}`
-  	document.body.prepend(div)
-  	const overlay = document.querySelector('.ws-modal__overlay')
+        // const $container = document.querySelector('#ws-v');
+        const $video = document.querySelectorAll(options.elements);
+        const $body = document.querySelector('body');
 
-  	function _close(event){
-   		const modalVideo = document.querySelector('.ws-modal__video')
-  		$body.classList.remove('hiden')
-  		modalVideo.remove()
-  		overlay.removeEventListener('click', _close)
-  	}
-  	overlay.addEventListener('click', _close)
-	}
+        // }
+        const renderBlock = function(el) {
+            /**
+             * Отрисовывает все привью на странице
+             * id берется с дата атрибута ссылки
+             */
+            if($video){
+                let lenghtId = el.dataset.wsVblock;
+            let idV = lenghtId.split('/');
+            let lengthObj = idV.length - 1;
+            let id = idV[lengthObj];
 
-	// Функция рендера превью
-	function _renderBlock(container, src='', alt=''){
-		let videoImgBlock = `
-			<div class="ws-modal__wrap">
-          <div class="ws-video__icon">
-              <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube" class="svg-inline--fa fa-youtube fa-w-18" role="img" viewBox="0 0 576 512">
-                  <path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" /></svg>
-              <img src="http://img.youtube.com/vi/${id || ''}/hqdefault.jpg" alt="${alt || ''}" class="ws-video__img">
-          </div>
-      </div>
-	  `
+            let alt = 'prevue'
+            let videoImgBlock = `
+                    <div class="ws-video-modal__overlay">
+                        <div class="ws-video-modal__content-icon">
+                        <div class='ws-video-modal__icon-play'></div>
+                    </div>
+                    <img class="ws-video-modal__preview" src="http://img.youtube.com/vi/${id || ''}/hqdefault.jpg" alt="${options.alt || 'preview video'}">
+            `
+            let div = document.createElement('div')
+            div.className = "ws-video-modal__content"
+            div.innerHTML = `${videoImgBlock}`
+            el.prepend(div)
 
-	  let div = document.createElement('div')
-  	div.className = "ws-modal__wrap"
-  	div.innerHTML = `${videoImgBlock}`
-  	container.prepend(div)
-	}
+            } //if
+            
+        }
 
-	// Функция рендера модального окна
+        function eventClickVideoBlock(el){
+            el.addEventListener('click', (event)=>{
+                let lenghtId = event.path[3].dataset.wsVblock;
+                let idV = lenghtId.split('/');
+                let lengthObj = idV.length - 1;
+                let id = idV[lengthObj];
+                renderModal(id)
+            })
+        }
 
-	// Проверяем не пуст ли массив и запускаем рендер
-	if(options.langth !== 0){
-		// const container = document.querySelector(options.container);
-		for (var i = 0; i < options.length; i++) {
-			const container = document.querySelector(opnions[i].container)
-			if(!container){
-				return ''
-			}
-			widthVideo = opnions[i].widthVideo
-			heightVideo = opnions[i].widthVideo
-			_renderBlock(container, opnions[i].src, opnions[i].alt)
-		}
-		// Навешиваем события клика на превью
-		function _eventClick(){
-			const blockEvent = document.querySelectorAll('.ws-video__icon')
-			blockEvent.forEach(function(item) {
-				item.addEventListener('click', ()=>{
-					// console.log()
-					_renderModal()
-				})
-			})
-		}
-		_eventClick()
-	}
-}
-wsVideoModalInit(options = [
-	 {
-		container:'.slide-video',
-		src: 'efwL2aV54sY',
-		alt: 'video alt',
-		widthVideo: '870px',
-		heightVideo: '500px',
-		title: 'Title',
-		decription: 'Text Text Text Text Text Text Text Text Text Text Text Text',
-	},
-])
+        function itemsValue() {
+            if($video){
+                $video.forEach(function(item) {
+                renderBlock(item)
+                eventClickVideoBlock(item)
+                });
+            }
+            // Получаем наши блоки, куда надо рендерить превью.
+            
+        }
+
+        function renderModal(id = '2wQxF7gTcFo') {
+            let widthVideo = options.widthVideo;
+            let heightVideo = options.heightVideo;
+            let alt = options.alt;
+
+            let videoBlock = ` 
+                        <div class="ws-modal__v-top-overlay">
+                            <span class="ws-modal__close">&#10006;</span>
+							<div class="ws-modal__video-wrap">
+								<iframe class="ws-modal__iframe" style="width: ${widthVideo || '450px'}; height: ${heightVideo || '250px'};" src="https://www.youtube.com/embed/${id || ''}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							</div>
+						</div>
+						`
+            let div = document.createElement('div')
+            div.className = "ws-modal__video"
+            div.innerHTML = `${videoBlock}`
+            document.body.prepend(div)
+
+            $body.classList.add('scrol-none')
+            const close = document.querySelector('.ws-modal__close')
+            close.addEventListener('click', closeModal)
+        }
+        function closeModal(){
+            $body.classList.remove('scrol-none');
+            const close = document.querySelector('.ws-modal__close')
+            close.removeEventListener( "click", closeModal);
+            document.querySelector('.ws-modal__video').remove()
+        }
+        itemsValue(); //Выводит все картинки/превью
+    }
+
+
+    modalVideoConstructor({
+        elements: '.ws-video',
+        widthVideo: '870px',
+        heightVideo: '520px',
+        alt: 'preview',
+    });
+
 
 // Swiper
 const swiper = new Swiper('.swiper-container', {
@@ -133,3 +144,4 @@ footerBtn.forEach(function(item) {
 		setTimeout(deleteBtnEffects, 2000);
 	})
 });
+})
